7c5afbb968d9b6848e3b1e70486126bb8f7f6f30
lgklsv <kolosov20011@gmail.com>
2025-04-19 17:55:47 +0400

    Add cache for products request

--- bff_service/src/server.ts
+++ bff_service/src/server.ts
@@ -3,12 +3,27 @@ import formBody from "@fastify/formbody";
 import "dotenv/config";
 
 const server = Fastify();
-
 server.register(formBody);
 
 const serviceUrls: { [key: string]: string } = {
   cart: process.env.CART_SERVICE_URL!,
   product: process.env.PRODUCT_SERVICE_URL!,
+};
+
+interface CacheEntry {
+  data: any;
+  expiry: number;
+}
+
+const cacheStorage = new Map<string, CacheEntry>();
+
+interface CacheOptions {
+  expiryInSeconds: number;
+  methods?: string[];
+}
+
+const routeCacheConfig: Record<string, CacheOptions> = {
+  "/product/products": { expiryInSeconds: 120, methods: ["GET"] }, // 2 minutes
 };
 
 async function forwardRequest(request: FastifyRequest, reply: FastifyReply) {
@@ -21,8 +36,23 @@ async function forwardRequest(request: FastifyRequest, reply: FastifyReply) {
     return reply.status(502).send({ error: "Cannot process request" });
   }
 
-  const targetUrl = `${targetServiceUrl}${request.url.replace(`/${recipientServiceName}`, "")}`;
+  const targetPath = request.url.replace(`/${recipientServiceName}`, "");
+  const targetUrl = `${targetServiceUrl}${targetPath}`;
   const method = request.method;
+
+  const cacheConfig = routeCacheConfig[`/${recipientServiceName}${targetPath}`];
+
+  if (
+    cacheConfig &&
+    (!cacheConfig.methods || cacheConfig.methods.includes(method))
+  ) {
+    const cachedEntry = cacheStorage.get(targetUrl);
+    if (cachedEntry && Date.now() < cachedEntry.expiry) {
+      console.log(`Serving ${request.url} from cache`);
+      return reply.status(200).send(cachedEntry.data);
+    }
+  }
+
   const headers = { ...request.headers };
   delete headers["host"];
   delete headers["Host"];
@@ -64,6 +94,16 @@ async function forwardRequest(request: FastifyRequest, reply: FastifyReply) {
       return reply
         .status(response.status)
         .send({ error: responseData?.message || "Error from backend service" });
+    }
+
+    if (
+      cacheConfig &&
+      (!cacheConfig.methods || cacheConfig.methods.includes(method)) &&
+      response.ok
+    ) {
+      const expiry = Date.now() + cacheConfig.expiryInSeconds * 1000;
+      cacheStorage.set(targetUrl, { data: responseData, expiry });
+      console.log(`Cached ${request.url}`);
     }
 
     reply.status(response.status).send(responseData);
